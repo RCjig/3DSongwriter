39c77ae3bfea6c7eaa7c3ced707d258866b2ebea
RCjig <rcajigal97@gmail.com>
2019-03-07 13:44:41 -0800

    Made more scripts
    Also maybe got trigger for crossing a gate, but didn't test it

---
+++ Final/Assets/Scripts/ModeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeController : MonoBehaviour
+{
+    private string mode; // string cus idk how to define enums outside this script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mode = "EDIT";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeMode()
+    {
+        if (mode == "EDIT") mode = "TEST";
+        else if (mode == "TEST") mode = "PLAY";
+        else mode = "EDIT";
+    }
+
+    public string GetMode()
+    {
+        return mode;
+    }
+}
--- Final/Assets/Scripts/NoteGateController.cs
+++ Final/Assets/Scripts/NoteGateController.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OVR;
 
 public class NoteGateController : MonoBehaviour
 {
+    readonly float TRIGGER_BUFFER = 0.25f; // idk how else to check when a player crosses a line
+
+    private GameObject playerController;
+    private LineRenderer triggerLine;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.Find("OVRPlayerController");
+        triggerLine = this.GetComponentInChildren<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsLineTriggered()) OnLineTrigger();
+    }
+
+    private bool IsLineTriggered()
+    {
+        return (Mathf.Abs(playerController.transform.position.z - triggerLine.transform.position.z) < TRIGGER_BUFFER) ? true : false;
+    }
+
+    private void OnLineTrigger()
+    {
+        // play notes
     }
 }
